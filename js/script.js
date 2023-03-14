f34f907020ee9907aeaf56429189d51d4f4abe14
ShaMAD <at.shamad@gmail.com>
2023-03-14 21:36:57 +0300

    Задание 1

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let num1 = +prompt("Введите число 1");
+let num2 = +prompt("Введите число 2");
+
+(num1 <= 1 && num2 >= 3) ? alert("num1 >= 1 и num2 >= 3") : alert(`Вы ввели число 1: ${num1} и число 2: ${num2}`);
